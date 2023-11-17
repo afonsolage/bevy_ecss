f2514ff50439fcc32a56c4a3f5707f6574a6849e
Piotr Siuszko <siuszko@zoho.com>
2023-11-17 17:34:16 +0100

    Format

--- src/property/colors.rs
+++ src/property/colors.rs
@@ -21,14 +21,14 @@ pub(super) fn parse_hex_color(hex: &str) -> Option<Color> {
 /// Accepts any [valid CSS named-colors](https://developer.mozilla.org/en-US/docs/Web/CSS/named-color).
 pub(super) fn parse_named_color(name: &str) -> Option<Color> {
     if let Ok(cssparser::Color::RGBA(cssparser::RGBA {
-                                         red,
-                                         green,
-                                         blue,
-                                         alpha,
-                                     })) =cssparser::parse_color_keyword(name)
-        {
-            Some(Color::rgba_u8(red, green, blue, alpha))
-        } else {
-            None
-        }
+        red,
+        green,
+        blue,
+        alpha,
+    })) = cssparser::parse_color_keyword(name)
+    {
+        Some(Color::rgba_u8(red, green, blue, alpha))
+    } else {
+        None
+    }
 }
