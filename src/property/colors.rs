21e5101a2531f52427808d2ea4ae9b0cf054a938
Afonso Lage <lage.afonso@gmail.com>
2023-11-19 07:25:59 -0300

    Upgraded cssparser to 0.33

--- src/property/colors.rs
+++ src/property/colors.rs
@@ -1,22 +1,11 @@
 use bevy::prelude::Color;
 
-fn to_bevy_color(css_color: Option<cssparser::Color>) -> Option<Color> {
-    // TODO: Implement other colors type
-    if let Some(cssparser::Color::Rgba(cssparser::RgbaLegacy {
-        red,
-        green,
-        blue,
-        alpha,
-    })) = css_color
-    {
-        Some(Color::rgba_u8(red, green, blue, (alpha * 255.0) as u8))
+pub(super) fn parse_hex_color(hex: &str) -> Option<Color> {
+    if let Ok((r, g, b, a)) = cssparser::color::parse_hash_color(hex.as_bytes()) {
+        Some(Color::rgba_u8(r, g, b, (a * 255.0) as u8))
     } else {
         None
     }
-}
-
-pub(super) fn parse_hex_color(hex: &str) -> Option<Color> {
-    to_bevy_color(cssparser::parse_hash_color(hex.as_bytes()).ok())
 }
 
 // Source: https://developer.mozilla.org/en-US/docs/Web/CSS/named-color
@@ -25,5 +14,15 @@ pub(super) fn parse_hex_color(hex: &str) -> Option<Color> {
 ///
 /// Accepts any [valid CSS named-colors](https://developer.mozilla.org/en-US/docs/Web/CSS/named-color).
 pub(super) fn parse_named_color(name: &str) -> Option<Color> {
-    to_bevy_color(cssparser::parse_color_keyword(name).ok())
+    if let Ok(cssparser_color::Color::Rgba(cssparser_color::RgbaLegacy {
+        red,
+        green,
+        blue,
+        alpha,
+    })) = cssparser_color::parse_color_keyword(name)
+    {
+        Some(Color::rgba_u8(red, green, blue, (alpha * 255.0) as u8))
+    } else {
+        None
+    }
 }
